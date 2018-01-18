e4f47e4ce0f1512ef59b52dd53c4df7955aa3af9
Mark Gregan <mark@gregan.org>
2018-01-17 21:16:58 -0500

    Creating and styling home join form

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <form action="#" class="join">
+      <h1>Start Chatting</h1>
+
+      <label form="room" class="label">Room ID</label>
+      <input type="text" name="room" class="input" placeholer="Ex: TechChat" />
+
+      <label form="name" class="label">Your name</label>
+      <input type="text" name="name" class="input" />
+
+      <button type="submit" class="button button--primary">
+        Join Chat
+      </button>
+    </form>
+  </div>
+</template>
+
+<style lang="scss">
+  .home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+  .join {
+    width: 100%;
+    max-width: 400px;
+  }
+</style>
+
+<script>
+
+export default {
+  name: 'Home',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
---
+++ src/store/getters.js
@@ -0,0 +1,3 @@
+export const getRoom = (state) => {
+  return state.room
+}
