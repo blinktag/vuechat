ac0e062cded244fa0d442d484c155c8347facbee
Mark Gregan <mark@gregan.org>
2018-01-18 20:03:33 -0500

    Layout and styling of room view

--- src/components/room/index.vue
+++ src/components/room/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <p>room {{ room }}</p>
+  <div class="room">
+    <div class="people" id="people">
+      <div class="person" v-for="x in 10">
+        <div class="person__video">
+          <video></video>
+        </div>
+        <div class="person__name">
+          Mark
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +21,74 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.room {
+  height: 100%;
+  position: relative;
+}
 
+.people {
+  display: flex;
+  flex-wrap: wrap;
+  vertical-align: baseline;
+  width: 100%;
+  padding: 40px;
+}
+
+video {
+  width: 100%;
+}
+
+.person {
+  position: relative;
+  margin: 0 1% 60px 1%;
+  width: 100%;
+
+  &__video {
+    background-color: #ccc;
+    margin-bottom: 10px;
+    box-shadow: 0 10px 25px rgba(49,49,49, .2);
+  }
+
+  &__name {
+    text-align:center;
+    font-weight: 600;
+  }
+
+  @media (min-width: 576px) and (max-width: 767px) {
+    width: 49%;
+
+    &:nth-of-type(2n) {
+      margin-right: 0;
+    }
+
+    &:nth-of-type(2n + 1) {
+      margin-left: 0;
+    }
+  }
+
+  @media (min-width: 768px) and (max-width: 991px) {
+    width: 32%;
+
+    &:nth-of-type(3n) {
+      margin-right: 0;
+    }
+
+    &:nth-of-type(3n + 1) {
+      margin-left: 0;
+    }
+  }
+
+  @media (min-width: 992px) {
+    width: 23.5%;
+
+    &:nth-of-type(4n) {
+      margin-right: 0;
+    }
+
+    &:nth-of-type(4n + 1) {
+      margin-left: 0;
+    }
+  }
+}
 </style>
